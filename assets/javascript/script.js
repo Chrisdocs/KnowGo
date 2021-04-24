1ec04a26e6496ba86caa23fe35e456fad2e2c1db
Chris Wolfe <chrisa.wolfe@gmail.com>
2021-04-24 15:49:58 -0500

    weather api connected

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -1,8 +1,8 @@
 // Write a fetch request to the Giphy API
-var weatherAPI = "api.openweathermap.org/data/2.5/weather?q={48}&appid=53cd6e2725805df5b134360f4870a02f"
+var weatherAPI = "https://api.openweathermap.org/data/2.5/weather?q=austin,US-TX&appid=53cd6e2725805df5b134360f4870a02f"
 // Then log the response in the console
 fetch(weatherAPI).then(function(response){
     response.json().then(function(data) {
         console.log(data);
-    })
+    });
 })
